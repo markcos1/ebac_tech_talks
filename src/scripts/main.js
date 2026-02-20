72305f5cd653a17e7a8fbea1e98e4c04016478f9
marcos <marcos@exemplo.com>
2026-02-19 21:26:01 -0300

    novo commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
-console.log("Olá mundo");
+AOS.init();
+
+const dataDoEvento = new Date("May 31, 2026 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contador = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasAteOEvento =distanciaAteOEvento / (1000 * 60 * 60 * 24);
+    console.log(diasAteOEvento);
+}, 1000);
